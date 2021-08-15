70db5c9dc0149c9f00f329b213b7cfb0e19de1d7
Yang YU <yuyangyyy@gmail.com>
2021-08-15 16:26:50 +0200

    use jsx and mapping array emojis and use condtion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
 
+const displayEmojiName = (event) => alert(event.target.id);
+const emojis = [
+  {
+    emoji: '😀',
+    name: 'grinning face',
+  },
+  {
+    emoji: '🎉',
+    name: 'party popper',
+  },
+  {
+    emoji: '💃',
+    name: 'woman dancing',
+  },
+];
+
 function App() {
+  const greeting = 'greeting';
+  const displayAction = true;
+  // creer variable en conditionnel si ture et affichier i am writing jsx
+  // si condition est false alors n'affiche pas la p
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 id={greeting}> Hello le monde</h1>
+      {displayAction && <p>I am writing JSX</p>}
+      <ul>
+        {emojis.map((emoji) => (
+          <li key={emoji.name}>
+            <button onClick={displayEmojiName}>
+              <div id={emoji.name}>{emoji.emoji}</div>
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+// function App() {
+//   const greeting = 'greetingggg';
+//   return (
+//     <div className="container">
+//       <h1 id="greeting">Hello World</h1>
+//       <p>I am writing JSX</p>
+
+//       <ul>
+//         <li>
+//           <button onClick={displayEmojiName}>
+//             <span role="img" aria-label="grinning face" id="grinning face">
+//               😀
+//             </span>
+//           </button>
+//         </li>
+//         <li>
+//           <button onClick={displayEmojiName}>
+//             <span role="img" aria-label="party popper" id="party popper">
+//               🎉
+//             </span>
+//           </button>
+//         </li>
+//         <li>
+//           <button onClick={displayEmojiName}>
+//             <span role="img" aria-label="woman dancing" id="woman dancing">
+//               💃
+//             </span>
+//           </button>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// }
 
 export default App;
